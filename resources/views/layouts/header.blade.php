82fb88df839fef07c74a2d225898cccf03d32dd5
gagan <gagandeepk.610weblab@gmail.com>
2024-09-12 16:52:05 +0530

    Logo Updated

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -64,7 +64,8 @@
     <div class="container-fluid p-0">
         <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-lg-5">
             <a href="/" class="navbar-brand ml-lg-3">
-                <h1 class="m-0 display-5 text-uppercase text-primary"><i class="fa fa-truck mr-2"></i>Aims Alberta</h1>
+                <!-- <h1 class="m-0 display-5 text-uppercase text-primary"><i class="fa fa-truck mr-2"></i>Aims Alberta</h1> -->
+                 <img src="{{ asset('img/aims-logo.png') }}" class="img-fluid" alt="Responsive image" width="50%" height="50%">
             </a>
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
