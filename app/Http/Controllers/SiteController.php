af32dd2e4529fc5b34c914c636299741153d70a3
gagan <gagandeepk.610weblab@gmail.com>
2024-09-12 12:51:54 +0530

    Code Init

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function home(){
+        return view('pages.home');
+    }
+
+    public function about(){
+        return view('pages.about');
+    }
+
+    public function contact(){
+        return view('pages.contact');
+    }
+
+    public function price(){
+        return view('pages.price');
+    }
+
+    public function service(){
+        return view('pages.service');
+    }
+
+    public function single(){
+        return view('pages.single');
+    }
+
+    public function blog(){
+        return view('pages.blog');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [SiteController::class, 'home']);
+Route::get('/about', [SiteController::class, 'about']);
+Route::get('/blog', [SiteController::class, 'blog']);
+Route::get('/contact', [SiteController::class, 'contact']);
+Route::get('/price', [SiteController::class, 'price']);
+Route::get('/service', [SiteController::class, 'service']);
+Route::get('/single', [SiteController::class, 'single']);
