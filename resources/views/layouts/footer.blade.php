febef04896be2119ad5cadffcd999f8f319207a5
gagan <gagandeepk.610weblab@gmail.com>
2024-09-20 11:15:48 +0530

    Css

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -52,12 +52,19 @@
     <div class="container-fluid bg-dark text-white border-top py-4 px-sm-3 px-md-5"
         style="border-color: #3E3E4E !important;">
         <div class="row">
-            <div class="col-lg-6 text-center text-md-left mb-3 mb-md-0">
+            <div class="col-lg-4 text-center text-md-left mb-3 mb-md-0">
                 <p class="m-0 text-white">&copy; <a href="#">Aims Alberta</a>. All Rights Reserved.
 
                 </p>
             </div>
-            <div class="col-lg-6 text-center text-md-right">
+            <div class="col-lg-4 text-center text-md-left mb-3 mb-md-0">
+                <p class="m-0 text-white">
+                <i class="fa-solid fa-building"></i>    
+                Version: {{ env('APP_VERSION') }}
+
+                </p>
+            </div>
+            <div class="col-lg-4 text-center text-md-right">
                 <ul class="nav d-inline-flex">
                     <li class="nav-item">
                         <a class="nav-link text-white py-0" href="#">Privacy</a>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -31,9 +31,10 @@
     <div class="container-fluid bg-dark">
         <div class="row py-2 px-lg-5">
             <div class="col-lg-6 text-center text-lg-left mb-2 mb-lg-0">
-                <div class="d-inline-flex align-items-center text-white">
+                <div class="align-items-center text-white">
                     <small><i class="fa fa-phone-alt mr-2"></i>+1 780 3401762</small>
                     <small class="px-3">|</small>
+                   
                     <small><i class="fa fa-envelope mr-2"></i>info@aimsalbertalogistics.com</small>
                 </div>
             </div>
