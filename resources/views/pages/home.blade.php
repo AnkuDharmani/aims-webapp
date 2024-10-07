3182b2c8521dc5bedf4abbf404bfe5a41d964d02
Anku <anku24d@gmail.com>
2024-10-07 21:51:46 +0530

    home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,8 +2,8 @@
 <!-- Header Start -->
 <div class="jumbotron jumbotron-fluid mb-5">
     <div class="container text-center py-5">
-        <h1 class="text-primary mb-4">Safe & Faster</h1>
-        <h1 class="text-white display-3 mb-5">Logistics Services</h1>
+        <h1 class="text-primary mb-4">Canada’s Premier End-to-End </h1>
+        <h1 class="text-white display-3 mb-5">Logistics Service Provider</h1>
         <div class="mx-auto" style="width: 100%; max-width: 600px;">
             <!-- <div class="input-group">
                 <input type="text" class="form-control border-light" style="padding: 30px;" placeholder="Tracking Id">
@@ -30,7 +30,10 @@
             <div class="col-lg-7">
                 <h6 class="text-primary text-uppercase font-weight-bold">About Us</h6>
                 <h1 class="mb-4">Trusted & Faster Logistic Service Provider</h1>
-                <p class="mb-4">At Aims Alberta Logistics, we specialize in providing efficient and reliable logistics solutions tailored to meet the unique needs of our clients. With years of experience in the industry, our dedicated team is committed to ensuring timely deliveries, streamlined operations, and exceptional customer service</p>
+                <p class="mb-4">At Aims Alberta Logistics, we specialize in providing efficient and reliable logistics
+                    solutions tailored to meet the unique needs of our clients. With years of experience in the
+                    industry, our dedicated team is committed to ensuring timely deliveries, streamlined operations, and
+                    exceptional customer service</p>
             </div>
         </div>
     </div>
@@ -60,26 +63,28 @@
 <div class="container-fluid bg-secondary my-5">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-lg-7 py-5 py-lg-0">
+            <div class="col-lg-12 py-5 pt-py-lg-0 px-5">
                 <h6 class="text-primary text-uppercase font-weight-bold">Get A Quote</h6>
                 <h1 class="mb-4">Request A Free Quote</h1>
-                <p class="mb-4">Looking for reliable and cost-effective logistics solutions? Get in touch with us today! Simply fill out our form, and we’ll provide you with a customized quote tailored to your specific needs. No obligations, just seamless service and competitive pricing.</p>
+                <p class="mb-4">Looking for reliable and cost-effective logistics solutions? Get in touch with us
+                    today! Simply fill out our form, and we’ll provide you with a customized quote tailored to your
+                    specific needs. No obligations, just seamless service and competitive pricing.</p>
                 <div class="row">
                     <div class="col-sm-4">
-                        <h1 class="text-primary mb-2" data-toggle="counter-up">225</h1>
+                        <h1 class="text-primary mb-2" data-toggle="counter-up">25</h1>
                         <h6 class="font-weight-bold mb-4">SKilled Experts</h6>
                     </div>
                     <div class="col-sm-4">
-                        <h1 class="text-primary mb-2" data-toggle="counter-up">1050</h1>
+                        <h1 class="text-primary mb-2" data-toggle="counter-up">100</h1>
                         <h6 class="font-weight-bold mb-4">Happy Clients</h6>
                     </div>
                     <div class="col-sm-4">
-                        <h1 class="text-primary mb-2" data-toggle="counter-up">2500</h1>
+                        <h1 class="text-primary mb-2" data-toggle="counter-up">250</h1>
                         <h6 class="font-weight-bold mb-4">Complete Projects</h6>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-5">
+            {{-- <div class="col-lg-5">
                 <div class="bg-primary py-5 px-4 px-sm-5">
                     <form class="py-5">
                         <div class="form-group">
@@ -103,7 +108,7 @@
                         </div>
                     </form>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 </div>
@@ -118,38 +123,19 @@
             <h1 class="mb-4">Best Logistic Services</h1>
         </div>
         <div class="row pb-3">
-            <div class="col-lg-3 col-md-6 text-center mb-5">
-                <div class="d-flex align-items-center justify-content-center bg-primary mb-4 p-4">
-                    <i class="fa fa-2x fa-plane text-dark pr-3"></i>
-                    <h6 class="text-white font-weight-medium m-0">Air Freight</h6>
-                </div>
-                <p>Our air freight services offer fast, reliable, and secure transportation for your time-sensitive shipments. </p>
-                <a class="border-bottom text-decoration-none" href="">Read More</a>
-            </div>
-            <div class="col-lg-3 col-md-6 text-center mb-5">
-                <div class="d-flex align-items-center justify-content-center bg-primary mb-4 p-4">
-                    <i class="fa fa-2x fa-ship text-dark pr-3"></i>
-                    <h6 class="text-white font-weight-medium m-0">Ocean Freight</h6>
-                </div>
-                <p>Our ocean freight services provide cost-effective and dependable solutions for transporting large volumes of cargo worldwide.</p>
-                <a class="border-bottom text-decoration-none" href="">Read More</a>
-            </div>
-            <div class="col-lg-3 col-md-6 text-center mb-5">
+
+
+
+            <div class="col-lg-12 col-md-12 text-center mb-5" style=" margin: 0 27%; max-width: 51%; ">
                 <div class="d-flex align-items-center justify-content-center bg-primary mb-4 p-4">
                     <i class="fa fa-2x fa-truck text-dark pr-3"></i>
                     <h6 class="text-white font-weight-medium m-0">Land Transport</h6>
                 </div>
-                <p>Our land transport services ensure reliable, efficient, and flexible delivery of goods across local and regional routes. </p>
-                <a class="border-bottom text-decoration-none" href="">Read More</a>
+
             </div>
-            <div class="col-lg-3 col-md-6 text-center mb-5">
-                <div class="d-flex align-items-center justify-content-center bg-primary mb-4 p-4">
-                    <i class="fa fa-2x fa-store text-dark pr-3"></i>
-                    <h6 class="text-white font-weight-medium m-0">Cargo Storage</h6>
-                </div>
-                <p>Our secure and strategically located storage facilities provide flexible solutions for all your cargo needs.</p>
-                <a class="border-bottom text-decoration-none" href="">Read More</a>
-            </div>
+            <p>Our land transport services ensure reliable, efficient, and flexible delivery of goods across local
+                and regional routes. </p>
+            <a class="border-bottom text-decoration-none" href="">Read More</a>
         </div>
     </div>
 </div>
@@ -166,7 +152,9 @@
             <div class="col-lg-7 py-5 py-lg-0">
                 <h6 class="text-primary text-uppercase font-weight-bold">Why Choose Us</h6>
                 <h1 class="mb-4">Faster, Safe and Trusted Logistics Services</h1>
-                <p class="mb-4">we stand out with our commitment to excellence, reliability, and customer-focused solutions. With a global network, cutting-edge technology, and a team of logistics experts, we ensure your shipments are handled with precision and care.</p>
+                <p class="mb-4">we stand out with our commitment to excellence, reliability, and customer-focused
+                    solutions. With a global network, cutting-edge technology, and a team of logistics experts, we
+                    ensure your shipments are handled with precision and care.</p>
                 <ul class="list-inline">
                     <li>
                         <h6><i class="far fa-dot-circle text-primary mr-3"></i>Best In Industry</h6>
@@ -351,7 +339,7 @@
 
 
 <!-- Blog Start -->
-<div class="container-fluid pt-5">
+{{-- <div class="container-fluid pt-5">
     <div class="container">
         <div class="text-center pb-2">
             <h6 class="text-primary text-uppercase font-weight-bold">Our Blog</h6>
@@ -416,6 +404,6 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 <!-- Blog End -->
 @include('layouts.footer')
